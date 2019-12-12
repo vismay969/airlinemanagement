8113a9ca41b55e124e3131be305341778888020f
vismay969 <vismayrshah@gmail.com>
2019-12-12 15:37:09 +0530

    Merge branches 'branch_mehernosh_dev01' and 'dev_nipun' of https://github.com/vismay969/airlinemanagement into dev_vismay
     Conflicts:
    	src/main/java/com/example/demo/controller/AirportController.java
    	src/main/java/com/example/demo/entity/AirportStruct.java
    	src/main/java/com/example/demo/entity/FlightMasterStruct.java
    	src/main/java/com/example/demo/repository/MyRepository.java

---
+++ src/main/java/com/example/demo/AirlinemanagementApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlinemanagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirlinemanagementApplication.class, args);
+
+    }
+
+}
