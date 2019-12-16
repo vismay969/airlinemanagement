d7307cfe785e3d011a17fb558dcc3fc5f0951791
ADMIN <mehernoshp@yahoo.com>
2019-12-16 16:58:53 +0530

    commit with user history and mail body 16-12

--- src/main/java/com/example/demo/util/Mailer.java
+++ src/main/java/com/example/demo/util/Mailer.java
@@ -1,5 +1,7 @@
 package com.example.demo.util;
 
+import com.example.demo.entity.BookinginfoStruct;
+import com.example.demo.entity.FlightMasterStruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -14,15 +16,20 @@ public class Mailer {
     @Autowired
     private JavaMailSender sender;
 
-    public String sendMail(String emailid) {
+    public String sendMail(String emailid, BookinginfoStruct bookinginfoStruct) {
         MimeMessage message = sender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message);
 
         try {
             helper.setTo("somjoy.bit@gmail.com");
+/*
+            helper.setTo(bookinginfoStruct.getCust_email());
+*/
 //            helper.setTo("somjoy.bit@gmail.com");
             helper.setText("Test email "+emailid+" and your interview is scheduled on at 11:00 AM");
-            helper.setSubject("Mail From Spring Boot");
+            helper.setText("Flight Booking Confirmation : Departure Date/Time : "+ bookinginfoStruct.getFlightDate() +
+                    " No of Seats : " + bookinginfoStruct.getNoOfPass() + " Cost Rs. "+ bookinginfoStruct.getTotal_fare() );
+            helper.setSubject("Your Ticket Booking Confirmation");
         } catch (MessagingException e) {
             e.printStackTrace();
             return "Error while sending mail ..";
